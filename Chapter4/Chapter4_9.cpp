e179c1d475564a84eb30f5feb91497dcabe4e27e
Joohyuck <joohyuck.seo@gmail.com>
2020-09-05 01:35:27 +0900

    Add the code of chapter 4.9
    : type aliases

---
+++ Chapter4/Chapter4_9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+
+
+
+int main()
+{
+	using namespace std;
+
+	//typedef double distance_t;
+
+	//double		my_distance;
+	//distance_t	home2work;
+	//distance_t	home2school;
+
+	//typedef vector<pair<string, int>> pairlist_t;
+	using pairlist_t = vector<pair<string, int>>;
+
+	pairlist_t pairlist1;
+	pairlist_t pairlist2;
+
+	return 0;
+}
