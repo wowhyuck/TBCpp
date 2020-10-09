fa1525c11553f3df538d35d6f06e5e861927ea89
Joohyuck <joohyuck.seo@gmail.com>
2020-10-09 16:45:20 +0900

    Add the code of chapter 8.12
    : friend

---
+++ Chapter8/Chapter8_12.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+//class B;		// forward declaration
+
+class A;
+
+class B
+{
+private:
+	int m_value = 2;
+
+public:
+	void doSomething(A& a);
+};
+
+class A
+{
+private:
+	int m_value = 1;
+
+	//friend class B;
+	friend void B::doSomething(A& a);
+};
+
+void B::doSomething(A& a)
+{
+	cout << a.m_value << endl;
+}
+
+
+int main()
+{
+	A a;
+	B b;
+	b.doSomething(a);
+
+	return 0;
+}
