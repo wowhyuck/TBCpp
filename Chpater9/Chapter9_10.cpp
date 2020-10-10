b6541a444a2c5e9f3d718f67cd0f7f7beb0c994f
Joohyuck <joohyuck.seo@gmail.com>
2020-10-10 18:14:15 +0900

    Add the code of chapter 9.10
    : converting constructor, explicit, and delete

---
+++ Chpater9/Chapter9_10.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+class Fraction
+{
+private:
+	int m_numerator;
+	int m_denominator;
+
+public:
+	Fraction(char) = delete;
+
+	explicit Fraction(int num = 0, int den = 1)
+		: m_numerator(num), m_denominator(den)
+	{
+		assert(den != 0);
+	}
+
+	Fraction(const Fraction& fraction)		// copy constructor
+		: m_numerator(fraction.m_numerator), m_denominator(fraction.m_denominator)
+	{
+		cout << "Copy constructor called" << endl;
+	}
+
+	friend ostream& operator << (ostream & out, const Fraction & f)
+	{
+		out << f.m_numerator << " / " << f.m_denominator << endl;
+		return out;
+	}
+};
+
+void doSomething(Fraction frac)
+{
+	cout << frac << endl;
+}
+
+
+int main()
+{
+	Fraction frac(7);
+
+	doSomething(frac);
+	//doSomething(7);			// if explicit is used at a constructor, it cannot be implemented.
+
+	//Fraction frac1('c');		// because of delete
+
+	return 0;
+}
