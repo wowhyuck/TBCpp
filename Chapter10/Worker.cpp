e5405331f4d02b9f87afc6ac03aa2ab3f3171dcc
Joohyuck <joohyuck.seo@gmail.com>
2020-10-12 00:24:05 +0900

    Add the code of chapter 10.5
    : dependencies

---
+++ Chapter10/Worker.cpp
@@ -0,0 +1,11 @@
+#include "Worker.h"
+#include "Timer.h"
+
+void Worker::doSomething()
+{
+	Timer timer;			// start timer
+
+							// do some work here
+
+	timer.elapsed();		// end timer and report elapsed time
+}
