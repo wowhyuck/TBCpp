cfc8a2f77e8da9de321a3d1f51727afc9378e4b9
Joohyuck <joohyuck.seo@gmail.com>
2020-10-11 19:11:50 +0900

    Add the code of chapter 10.2
    : composition

---
+++ Chapter10/Chapter10_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Monster.h"
+
+using namespace std;
+
+
+int main()
+{
+	Monster mon1("Sanson", Position2D(0, 0));
+
+	//while (1)		// game loop
+	{
+		// event
+		mon1.moveTo(Position2D(1, 1));
+		cout << mon1 << endl;
+
+	}
+
+	return 0;
+}
