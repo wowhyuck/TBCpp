555e347e44630ebf4efed8fb8f2c0aebb52315e1
Joohyuck <joohyuck.seo@gmail.com>
2020-10-13 19:26:20 +0900

    Add the code of chapter 12.7
    : pure virtual functions, abstract basic class, and interface class

---
+++ Chapter12/Chapter12_7.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+
+public:
+	Animal(std::string name)
+		: m_name(name)
+	{}
+
+	string getName() { return m_name; }
+
+	virtual void speak() const = 0;		// pure virtual function
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Meow " << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Woof " << endl;
+	}
+};
+
+class Cow : public Animal
+{
+public:
+	Cow(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Moooo " << endl;
+	}
+};
+
+/*----------------------------------------------*/
+/* Interface class */
+
+class IErrorLog
+{
+public:
+	virtual bool reportError(const char* errorMessage) = 0;
+
+	virtual ~IErrorLog() {}
+};
+
+class FileErrorLog : public IErrorLog
+{
+public:
+	bool reportError(const char* errorMessage) override
+	{
+		cout << "Writing error to a file" << endl;
+		return true;
+	}
+};
+
+class ConsoleErrorLog : public IErrorLog
+{
+public:
+	bool reportError(const char* errorMessage) override
+	{
+		cout << "Printing error to a console" << endl;
+		return true;
+	}
+};
+
+void doSomething(IErrorLog& log)
+{
+	log.reportError("Runtime Error!!");
+}
+
+
+int main()
+{
+	//Animal ani("Hi");
+
+	Cow cow("hello");
+
+	/*-----------------------*/
+
+	FileErrorLog file_log;
+	ConsoleErrorLog console_log;
+
+	doSomething(file_log);
+	doSomething(console_log);
+
+	return 0;
+}
