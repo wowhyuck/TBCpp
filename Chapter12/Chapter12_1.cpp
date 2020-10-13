226f89da34bb72024c9bb5d65c5c493b5b95a2a7
Joohyuck <joohyuck.seo@gmail.com>
2020-10-13 15:57:25 +0900

    Add the code of chapter 12.1
    : basic concept of polynorphism

---
+++ Chapter12/Chapter12_1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+
+public:
+	Animal(std::string name)
+		: m_name(name)
+	{}
+
+public:
+	string getName() { return m_name; }
+
+	virtual void speak() const
+	{
+		cout << m_name << " ??? " << endl;
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	Cat(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Meow " << endl;
+	}
+};
+
+class Dog : public Animal
+{
+public:
+	Dog(string name)
+		: Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << " Woof " << endl;
+	}
+};
+
+
+int main()
+{
+	Animal animal("my animal");
+	Cat cat("my cat");
+	Dog dog("my dog");
+
+	animal.speak();
+	cat.speak();
+	dog.speak();
+
+	Animal* ptr_animal1 = &cat;
+	Animal* ptr_animal2 = &dog;
+
+	ptr_animal1->speak();
+	ptr_animal2->speak();
+	cout << endl;
+
+	/*-----------------------------------*/
+
+	Cat cats[] = { Cat("cat1"), Cat("cat2") , Cat("cat3") , Cat("cat4") , Cat("cat5") };
+	Dog dogs[] = { Dog("dog1"), Dog("dog2") };
+
+	for (int i = 0; i < 5; i++)
+		cats[i].speak();
+
+	for (int i = 0; i < 2; i++)
+		dogs[i].speak();
+	cout << endl;
+	// ...
+
+	Animal* my_animals[] = { &cats[0], &cats[1], &cats[2], &cats[3], &cats[4],
+							&dogs[0], &dogs[1]
+	};
+
+	for (int i = 0; i < 7; i++)
+		my_animals[i]->speak();
+
+
+
+
+	return 0;
+}
