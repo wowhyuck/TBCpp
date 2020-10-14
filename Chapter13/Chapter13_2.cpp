5e7f17899e7a2b0b52b17edc4b6b0530b42c8556
Joohyuck <joohyuck.seo@gmail.com>
2020-10-14 17:40:56 +0900

    Add the code of chapter 13.3
    : template paramters which are non-type

--- Chapter13/Chapter13_2.cpp
+++ Chapter13/Chapter13_2.cpp
@@ -2,7 +2,18 @@
 
 int main()
 {
-	MyArray<double> my_array(10);
+	/* 13.2*/
+	//MyArray<double> my_array(10);
+
+	//for (int i = 0; i < my_array.getLength(); i++)
+	//	my_array[i] = i * 0.5;
+
+	//my_array.print();
+
+	/*--------------------------------------------------*/
+
+	/* 13.3*/
+	MyArray<double, 100> my_array;
 
 	for (int i = 0; i < my_array.getLength(); i++)
 		my_array[i] = i * 0.5;
--- Chapter13/MyArray.h
+++ Chapter13/MyArray.h
@@ -3,49 +3,87 @@
 #include <assert.h>		// for assert()
 #include <iostream>
 
-template<typename T>
+//template<typename T>
+//class MyArray
+//{
+//private:
+//	int m_length;
+//	T*	m_data;
+//
+//public:
+//	MyArray()
+//	{
+//		m_length = 0;
+//		m_data = nullptr;
+//	}
+//
+//	MyArray(int length)
+//	{
+//		m_data = new T[length];
+//		m_length = length;
+//	}
+//
+//	~MyArray()
+//	{
+//		reset();
+//	}
+//
+//	void reset()
+//	{
+//		delete[] m_data;
+//		m_data = nullptr;
+//		m_length = 0;
+//	}
+//
+//	T& operator[] (int index)
+//	{
+//		assert(index >= 0 && index < m_length);
+//		return m_data[index];
+//	}
+//
+//	int getLength()
+//	{
+//		return m_length;
+//	}
+//
+//	void print();
+//};
+//
+
+/* 13. 3*/
+
+template<typename T, unsigned int T_SIZE>
 class MyArray
 {
 private:
-	int m_length;
-	T*	m_data;
+	T* m_data;
 
 public:
 	MyArray()
 	{
-		m_length = 0;
-		m_data = nullptr;
-	}
-
-	MyArray(int length)
-	{
-		m_data = new T[length];
-		m_length = length;
+		m_data = new T[T_SIZE];
 	}
 
 	~MyArray()
 	{
-		reset();
-	}
-
-	void reset()
-	{
 		delete[] m_data;
-		m_data = nullptr;
-		m_length = 0;
 	}
 
 	T& operator[] (int index)
 	{
-		assert(index >= 0 && index < m_length);
+		assert(index >= 0 && index < T_SIZE);
 		return m_data[index];
 	}
 
 	int getLength()
 	{
-		return m_length;
+		return T_SIZE;
 	}
 
-	void print();
+	void print()
+	{
+		for (int i = 0; i < T_SIZE; i++)
+			std::cout << m_data[i] << " ";
+		std::cout << std::endl;
+	}
 };
-
