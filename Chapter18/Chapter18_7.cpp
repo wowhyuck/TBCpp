a4a9a7683995d38e2a9670a3d56cfe058c0e0c59
Joohyuck <joohyuck.seo@gmail.com>
2020-10-26 18:09:52 +0900

    Add the code of chapter 18.7
    : accessing a random position of a file

---
+++ Chapter18/Chapter18_7.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>		// exit()
+#include <sstream>
+
+using namespace std;
+
+
+int main()
+{
+	const string filename = "my_file.txt";
+
+	// make a file
+	{
+		ofstream ofs(filename);
+
+		for (char i = 'a'; i <= 'z'; i++)
+			ofs << i;
+		ofs << endl;
+	}
+
+	// read the file
+	//{
+	//	ifstream ifs(filename);
+
+	//	ifs.seekg(5);		// ifs.seekg(5, ios::beg);
+	//	cout << (char)ifs.get() << endl;
+
+	//	ifs.seekg(5, ios::cur);
+	//	cout << (char)ifs.get() << endl;
+
+	//	//ifs.seekg(-5, ios::end);
+
+	//	//ifs.seekg(0, ios::end);
+	//	//cout << ifs.tellg() << endl;
+
+	//	string str;
+	//	getline(ifs, str);
+
+	//	cout << str << endl;
+	//}
+
+	{
+		//fstream iofs(filename, ios::in | ios::out);
+		fstream iofs(filename);
+
+		iofs.seekg(5);
+		cout << (char)iofs.get() << endl;		// read
+
+		iofs.seekg(5);
+		iofs.put('A');		// write
+	}
+
+	return 0;
+}
