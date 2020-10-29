177b3323cde5048701f741c687ac6de4ea74e57d
Joohyuck <joohyuck.seo@gmail.com>
2020-10-29 18:14:43 +0900

    Add the code of chapter 19.5
    : async, future, promise

---
+++ Chapter19/Chapter19_5.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <future>
+#include <thread>
+
+using namespace std;
+
+
+int main()
+{
+	// multi-threading
+	//{
+	//	int result;
+	//	std::thread t([&] {result = 1 + 2; });
+	//	t.join();
+	//	cout << result << endl;
+	//}
+
+	//// task-based parallelism
+	//{
+	//	//std::future<int> fut = ...
+	//	auto fut = std::async([] {return 1 + 2; });
+	//	cout << fut.get() << endl;
+	//}
+
+	// future and promise
+	//{
+	//	std::promise<int> prom;
+	//	auto fut = prom.get_future();
+
+	//	auto t = std::thread([](std::promise<int>&& prom)
+	//	{
+	//		prom.set_value(1 + 2);
+	//	}, std::move(prom));
+	//	
+	//	cout << fut.get() << endl;
+	//	t.join();
+	//}
+
+	//{
+	//	auto f1 = std::async([] {
+	//		cout << "async1 start" << endl;
+	//		this_thread::sleep_for(chrono::seconds(2));
+	//		cout << "async1 end" << endl;
+	//	});
+
+	//	auto f2 = std::async([] {
+	//		cout << "async2 start" << endl;
+	//		this_thread::sleep_for(chrono::seconds(1));
+	//		cout << "async2 end" << endl;
+	//	});
+
+	//	std::cout << "Main function" << endl;
+	//}
+
+	//{
+	//	std::async([] {
+	//		cout << "async1 start" << endl;
+	//		this_thread::sleep_for(chrono::seconds(2));
+	//		cout << "async1 end" << endl;
+	//	});
+
+	//	std::async([] {
+	//		cout << "async2 start" << endl;
+	//		this_thread::sleep_for(chrono::seconds(1));
+	//		cout << "async2 end" << endl;
+	//	});
+
+	//	std::cout << "Main function" << endl;
+	//}
+
+	{
+		auto f1 = std::thread([] {
+			cout << "async1 start" << endl;
+			this_thread::sleep_for(chrono::seconds(2));
+			cout << "async1 end" << endl;
+		});
+
+		auto f2 = std::thread([] {
+			cout << "async2 start" << endl;
+			this_thread::sleep_for(chrono::seconds(1));
+			cout << "async2 end" << endl;
+		});
+
+		f1.join();
+		f2.join();
+
+		std::cout << "Main function" << endl;
+	}
+
+	return 0;
+}
