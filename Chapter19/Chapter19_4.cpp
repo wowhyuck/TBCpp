3482799c1051e022a22b0f61d18ca201ada8094d
Joohyuck <joohyuck.seo@gmail.com>
2020-10-29 17:47:44 +0900

    Add the code of chapter 19.4
    : race condition, std::atomic, and std::scoped_lock

---
+++ Chapter19/Chapter19_4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+mutex mtx;
+
+
+int main()
+{
+	//atomic<int> shared_memory(0);
+	int shared_memory(0);
+
+	auto count_func = [&]() {
+		for (int i = 0; i < 1000; i++)
+		{
+			this_thread::sleep_for(chrono::milliseconds(1));
+
+			//std::lock_guard lock(mtx);
+			std::scoped_lock lock(mtx);
+
+			shared_memory++;
+			//shared_memory.fetch_add(1);
+		}
+	};
+
+	thread t1 = thread(count_func);
+	thread t2 = thread(count_func);
+
+	t1.join();
+	t2.join();
+
+	cout << "After" << endl;
+	cout << shared_memory << endl;
+
+	return 0;
+}
